80aad64af2373ad500f56c28411f100577834a59
Banavath-devsingh1 <79032459+Banavath-devsingh1@users.noreply.github.com>
2021-02-14 09:15:46 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -5,12 +5,6 @@ int main()
 {
 
 
-    printf("%d \n",32);/// integer
-    printf("%ld \n,345678921");/// large integer
-    printf("%f \n",34.5);/// float
-    printf("%lf \n",345674.778);/// large float
-    printf("%c \n",'a');/// char
-    printf("vijayaANDdasru \n")/// string
-    printf("%x \n",12);/// hex
+    printf("helloworld");
     return 0;
 }
