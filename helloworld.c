795ebaf6d7e7bbdba1558e00082f443ee8877953
Banavath-devsingh1 <79032459+Banavath-devsingh1@users.noreply.github.com>
2021-02-14 09:14:00 +0530

    initial commit

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+
+    printf("%d \n",32);/// integer
+    printf("%ld \n,345678921");/// large integer
+    printf("%f \n",34.5);/// float
+    printf("%lf \n",345674.778);/// large float
+    printf("%c \n",'a');/// char
+    printf("vijayaANDdasru \n")/// string
+    printf("%x \n",12);/// hex
+    return 0;
+}
